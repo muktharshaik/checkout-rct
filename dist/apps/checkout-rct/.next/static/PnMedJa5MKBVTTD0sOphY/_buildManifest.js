ec352cb2fcb5414a5cdb7710b707aac6d9a627f3
muktharshaik <msshaikmukthar9@gmail.com>
2022-08-01 08:35:23 +0400

    dist

---
+++ dist/apps/checkout-rct/.next/static/PnMedJa5MKBVTTD0sOphY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/86-85c0bb53eddfd3cd.js","static/chunks/pages/index-3cbe86ac56a054fc.js"],"/_error":["static/chunks/pages/_error-e755336071b61fcc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
